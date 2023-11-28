1ebf5efda9e6ff4abe09f3999747bb472916f0ff
Nicolas-Evezard <127189747+Nicolas-Evezard@users.noreply.github.com>
2023-11-28 11:11:28 +0100

    Remove cache functionality and update error handling in weather controller

--- backend/app/controller/weather.js
+++ backend/app/controller/weather.js
@@ -1,40 +1,16 @@
 const weatherService = require("../services/weatherService");
-const cache = require("../cache");
-
-const clearCacheTwiceDaily = () => {
-  cache.clearCache();
-};
-
-setInterval(() => {
-  const now = new Date();
-  const hours = now.getHours();
-  const minutes = now.getMinutes();
-
-  if ((hours === 0 || hours === 12) && minutes === 0) {
-    clearCacheTwiceDaily();
-  }
-}, 60000);
 
 const getWeatherData = async (req, res) => {
   try {
     const { departmentCode } = req.body;
 
     if (!departmentCode) {
-      return res
-        .status(400)
-        .json({ message: "Code de département non valide" });
-    }
-
-    const weatherCache = cache.getCache();
-    if (weatherCache[departmentCode]) {
-      return res.status(200).json(weatherCache[departmentCode]);
+      return res.status(400).json({ message: "Code de département non valide" });
     }
 
     const weatherData = await weatherService.fetchWeatherDataByDepartmentCode(
       departmentCode
     );
-
-    weatherCache[departmentCode] = weatherData;
 
     res.status(200).json(weatherData);
   } catch (error) {
