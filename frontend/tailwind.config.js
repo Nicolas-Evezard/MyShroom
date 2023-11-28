242613cca52a02bbc8612ca0e39ce92467c17cd5
Nicolas-Evezard <127189747+Nicolas-Evezard@users.noreply.github.com>
2023-11-28 13:52:37 +0100

    Update title and add preline.js script

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,7 +6,38 @@ module.exports = {
     "node_modules/preline/dist/*.js",
   ],
   theme: {
-    extend: {},
+    colors: {
+      "primary": "#986041",
+          
+      "secondary": "#D08B2E",
+               
+      "accent": "#73a100",
+               
+      "neutral": "#4A342A",
+               
+      "base-100": "#FFE8D8",
+               
+      "info": "#0071ff",
+               
+      "success": "#5df173",
+               
+      "warning": "#f46b00",
+               
+      "error": "#c90a2d",
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      spacing: {
+        '8xl': '96rem',
+        '9xl': '128rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    }
   },
   plugins: [require("@tailwindcss/forms"), require("preline/plugin")],
 };
